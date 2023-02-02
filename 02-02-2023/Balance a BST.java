bb0e6b3fc0c1e2d6a8cdfe82478629cff2925196
DURGA PAVAN PENDYALA <85378789+Pavan555556@users.noreply.github.com>
2023-02-02 17:05:44 +0530

    Add files via upload

---
+++ 02-02-2023/Balance a BST.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode balanceBST(TreeNode root) {
+        List<Integer> arr = new ArrayList();
+        InOrder( root,  arr);
+        return sortedArrayToBST( arr, 0, arr.size()-1);
+    }
+    
+    public void InOrder(TreeNode node, List<Integer> arr){
+        if(node != null){
+            InOrder( node.left, arr);
+            arr.add(node.val);
+            InOrder( node.right, arr);
+        }
+    }
+    
+    public TreeNode sortedArrayToBST(List<Integer> arr, int start, int end) {
+
+        if (start > end) {
+            return null;
+        }
+ 
+        int mid = (start + end) / 2;
+        
+        TreeNode node = new TreeNode(arr.get(mid));
+        node.left = sortedArrayToBST(arr, start, mid - 1);
+        node.right = sortedArrayToBST(arr, mid + 1, end);
+         
+        return node;
+    }
+}

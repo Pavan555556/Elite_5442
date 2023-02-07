b385e21677584860f2c7abcfeca5d5e5e2423d71
DURGA PAVAN PENDYALA <85378789+Pavan555556@users.noreply.github.com>
2023-02-07 23:18:18 +0530

    Add files via upload

---
+++ 07-02-2023/keysandrooms.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(0);
+        helper(rooms, set, 0);
+        return set.size()==rooms.size();
+    }
+
+    public void helper(List<List<Integer>> rooms, HashSet<Integer> set , int curr){
+        for(int next: rooms.get(curr)){
+            if(!set.contains(next)){
+                set.add(next);
+                helper(rooms, set, next);
+            }
+        }
+    }
+}
